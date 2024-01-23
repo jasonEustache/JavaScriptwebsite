b8f23771ed2a676f251df5c8106d9bd1368a209b
jason eustache <jasoneustache.webdeveloper@gmail.com>
2024-01-23 09:17:52 -0500

    version one

---
+++ js/carousel.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll(".review-item");
+const buttons = document.querySelectorAll(".slide-ctrl-container button");
+
+let current = Math.floor(Math.random() * slides.length);
+let next = current < slides.length - 1 ? current + 1 : 0;
+let prev = current > 0 ? current - 1 : slides.length - 1;
+
+const dummySlides = [
+  //slide 0
+  //slide 1
+  //slide 2 -current [next] = current +1
+  //slide 3 -current
+];
+
+/*create evenListener for prev/next */
+
+//goToNext()
+//gotToPrev()
+//updateIndexes(param)
+//updateCSS()2
+// -- decide how to call prev/next
+//-- update variables
+//-- [current] = newIndex
+// -- [next] current + 1 : 0
+//[prev] current -1 :length - 1
+// update the css
+
+const update = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active", "prev", "next");
+  });
+  slides[current].classList.add("active");
+  slides[current].classList.add("prev");
+  slides[current].classList.add("next");
+};
+
+const goToNum = (number) => {
+  current = number;
+  next = current < slides.length - 1 ? current + 1 : 0;
+  prev = current > 0 ? current - 1 : slides.length - 1;
+  update();
+};
+
+const goToNext = () =>
+  current < slides.length - 1 ? gotTo(current + 1) : goToNum(0);
+const goToPrev = () =>
+  current > 0 ? goToNum(current - 1) : goToNum(slides.length - 1);
+
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener("click", () => {
+    i === 0 ? gotToPrev() : goToNext();
+  });
+}
+
+update();
---
+++ js/main.js
@@ -0,0 +1,140 @@
+const theme = "theme";
+const dataTheme = "data-theme";
+const themeTab = ".theme-tab";
+const switcherBtn = ".switcher-btn";
+const dark = "dark";
+const light = "light";
+const open = "open";
+const active = "active";
+const modalOpen = "[data-open]";
+const modalClose = "[data-close]";
+const isVisible = ".is-visible";
+const dataFilter = "[data-filter]";
+const portFolioData = "[data-item]";
+const root = document.documentElement;
+//theme
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+/*portfolio */
+const filterLink = document.querySelectorAll(dataFilter);
+const portfolioItems = document.querySelectorAll(portfolioData);
+const searchBox = document.querySelector("#search");
+//modal
+const openModal = document.querySelectorAll(modalOpen);
+const closeModal = document.querySelectorAll(modalClose);
+
+const seActive = (elm, selector) => {
+  if (document.querySelector(`${selector}.${active}`) !== null) {
+    document.querySelector(`${selector}.${active}`).classList.remove(active);
+  }
+  elm.classList.add(active);
+};
+
+const setTheme = (val) => {
+  if (val == dark) {
+    root.setAttribute(dataTheme, dark);
+    localStorage.setItem(theme, dark);
+  } else {
+    root.setAttribute(dataTheme, light);
+    localStorage.setItem(theme, light);
+  }
+};
+
+if (currentTheme) {
+  root.setAttribute(dataTheme, currentTheme);
+  switcher.forEach((btn) => {
+    btn.classList.remove(active);
+  });
+  if (currentTheme === dark) {
+    switcher[1].classList.add(active);
+  } else {
+    switcher[0].classList.add(active);
+  }
+}
+
+toggleTheme.addEventListener("click", function () {
+  const tab = this.parentElement.parentElement;
+  if (!tab.className.includes(open)) {
+    tab.classList.add(open);
+  } else {
+    tab.classList.add(open);
+  }
+});
+
+for (const elm of switcher) {
+  elm.addEventListener("click", function () {
+    const toggle = this.dataset.toggle;
+    setActive(elm, switcherBtn);
+    setTheme(toggle);
+  });
+}
+
+searchBox.addEventListener("keyup", (e) => {
+  const searchInput = e.target.value.toLowerCase().trim();
+  portfolioItems.forEach((item) => {
+    if (card.dataset.item.includes(searchInput)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
+
+for (const link of filterLink) {
+  link.addEventListener("click", function () {
+    setActive(link, ".filter-link");
+    const filter = this.dataset.filter;
+    portfolioItems.forEach((card) => {
+      if (filter === "all") {
+        card.style.display = "block";
+      } else if (card.dataset.item === filter) {
+        card.style.display = "block";
+      } else {
+        card.style.display = " none";
+      }
+    });
+  });
+}
+
+//modal/full site modal 'open buttons'
+
+for (const elm of openModal) {
+  elm.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const elm of closeModal) {
+  elm.addEventListener("click", function () {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+//modal
+
+document.addEventListener("click", (e) => {
+  // console.log(e.target, document.querySelector(".modal.is-visible"));
+  if (e.target === document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  // console.log(e.target, document.querySelector(".modal.is-visible"));
+  if (e.key === "Escape") {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+const elmsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elms-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elms", marqueeContent.children.length);
+
+for (let i = 0; i < elmsDisplayed; i += 1) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
